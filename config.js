9253b18b2e740d1316fb637964cdf32c9b3353f2
Wesley Fernandes <fernandess.weslley@gmail.com>
2022-10-24 22:05:41 -0300

    firebase config

--- config.js
+++ config.js
@@ -4,13 +4,13 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB1GPA3gyJJB4xtJF7PbRD8GfMdgf4GPy0",
-  authDomain: "helpdesk-v2-50cf1.firebaseapp.com",
-  projectId: "helpdesk-v2-50cf1",
-  storageBucket: "helpdesk-v2-50cf1.appspot.com",
-  messagingSenderId: "438676241244",
-  appId: "1:438676241244:web:e37df89e46f4f6623e9f99",
-  measurementId: "G-FSJEHTRCX2"
+  apiKey: "AIzaSyCI56aZlIE8zjMqIzWvtmih79ZFT7EKrf0",
+  authDomain: "helpdesk-90e51.firebaseapp.com",
+  projectId: "helpdesk-90e51",
+  storageBucket: "helpdesk-90e51.appspot.com",
+  messagingSenderId: "898735093560",
+  appId: "1:898735093560:web:98cb603c2e750cb581b3cd",
+  measurementId: "G-2D42TMG13L"
 }
 
 let app;
